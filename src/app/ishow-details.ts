a4608a9d810bd108e01d56f4187e16dc46cd6b31
marisadiaz <Risa.diaz@gmail.com>
2024-08-20 18:36:57 -0400

    updated show-details component ts, css, html

--- src/app/ishow-details.ts
+++ src/app/ishow-details.ts
@@ -1,7 +1,7 @@
 export interface IShowDetails {
     name: string
-    yearStart: Date
-    yearEnd: Date
+    yearStart: number //changed from Date to number to store only the year
+    yearEnd: number //changed from Date to number to store only the year
     description: string
     image: string 
 }
--- src/app/show-details/show-details.component.ts
+++ src/app/show-details/show-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IShowDetails } from '../ishow-details';
 
 @Component({
   selector: 'app-show-details',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './show-details.component.css'
 })
 export class ShowDetailsComponent {
+  current: IShowDetails = {
+    name: 'Friends',
+    yearStart: 1994, //When ready to fetch API data, change to 0
+    yearEnd: 2004, //When ready to fetch API data, change to 0
+    description: 'Six young (20-something) people from New York City (Manhattan), on their own and struggling to survive in the real world, find the companionship, comfort and support they get from each other to be the perfect antidote to the pressures of life.',
+    image: 'https://static.tvmaze.com/uploads/images/medium_portrait/41/104565.jpg'
+  }
+
 
 }
