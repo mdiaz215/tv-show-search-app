2bf64b3c3aa5eba17f49c8b10643ac9571bf2b35
mariiapdavey <mariiapdavey@gmail.com>
2024-08-19 19:31:32 -0700

    generated ShowDetails component

---
+++ src/app/show-details/show-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-details.component.html',
+  styleUrl: './show-details.component.css'
+})
+export class ShowDetailsComponent {
+
+}
