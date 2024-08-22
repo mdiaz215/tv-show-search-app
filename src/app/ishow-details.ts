95540023f5878606fed3798d12809f2f654532e6
marisadiaz <Risa.diaz@gmail.com>
2024-08-22 12:17:40 -0400

    removed comment

--- src/app/ishow-details.ts
+++ src/app/ishow-details.ts
@@ -1,6 +1,6 @@
 export interface IShowDetails { //This is the data we want to show.
     showName: string //changed from name to showName
-    yearStart: number, //changed to number to only collect year, but may revert back to Date
+    yearStart: number, //changed to number to only collect year
     yearEnd: number, 
     description: string
     image: string 
